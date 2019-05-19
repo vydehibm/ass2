71085e6ec8261f01c460e9d16b88acdc6f823e47
learner <learner@github.com>
2019-05-19 03:00:21 -0400

    Customer Service Complete & functional :)

--- src/main/java/com/frosters/controller/CustomerController.java
+++ src/main/java/com/frosters/controller/CustomerController.java
@@ -3,15 +3,16 @@ package com.frosters.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
 
 import com.frosters.model.Customer;
 import com.frosters.service.CustomerService;
@@ -23,7 +24,7 @@ public class CustomerController {
 	@Autowired
 	private CustomerService customerService;
 	
-	@RequestMapping(value = "/customer/", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<Customer>> getAllCustomersService(){
 
 		if (customerService.getAllCustomers().size() > 0)
@@ -33,13 +34,28 @@ public class CustomerController {
 	}
 	
 	@RequestMapping(path = "/{id}")
-	public Customer getSpecificCustomerService(@PathVariable("id") long customerId) {
-		return customerService.getSpecificCustomer(customerId).isPresent() ? customerService.getSpecificCustomer(customerId).get() : null;
+	public ResponseEntity<Customer> getSpecificCustomerService(@PathVariable("id") long customerId) {
+		return customerService.getSpecificCustomer(customerId);
 	}
 	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Customer> addCustomerService(@RequestBody Customer customer) {
+		return new ResponseEntity<Customer>(customerService.addCustomer(customer), HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(path = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Customer> updateExistingCustomerService(@RequestBody Customer customer, @PathVariable("id") long customerId) {
+		return customerService.updateExistingCustomer(customer, customerId);
+	}
 
-
-
-
-
+	@RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<HttpStatus> deleteSpecificCustomer(@PathVariable("id") long customerId){
+		return customerService.deleteSpecificCustomer(customerId);
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE)
+	public ResponseEntity<HttpStatus> deleteAllCustomersService(){
+		return customerService.deleteAllCustomers();
+	}
+	
 }
--- src/main/java/com/frosters/service/CustomerService.java
+++ src/main/java/com/frosters/service/CustomerService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.frosters.model.Customer;
@@ -19,8 +21,39 @@ public class CustomerService {
 	public List<Customer> getAllCustomers(){
 		return customerRepository.findAll();
 	}
-
-	public Optional<Customer> getSpecificCustomer(long customerId) {
-		return customerRepository.findById(customerId);
+//.isPresent() ? customerService.getSpecificCustomer(customerId).get() : null;
+	public ResponseEntity<Customer> getSpecificCustomer(long customerId) {
+		if (customerRepository.findById(customerId).isPresent())
+			return new ResponseEntity<>(customerRepository.findById(customerId).get(), HttpStatus.OK);
+		else
+		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
 	}
+
+	public Customer addCustomer(Customer customer) {
+		return customerRepository.save(customer);
+	}
+	
+	public ResponseEntity<Customer> updateExistingCustomer(Customer customer, long customerId) {
+		if (customerRepository.findById(customerId).isPresent())
+			return new ResponseEntity<>(customerRepository.save(customer), HttpStatus.OK);
+		else
+		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+	}
+	
+	public ResponseEntity<HttpStatus> deleteSpecificCustomer(long customerId) {
+		if (customerRepository.findById(customerId).isPresent()){
+			customerRepository.deleteById(customerId);
+			return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+		}
+		else
+		return new ResponseEntity<HttpStatus>(HttpStatus.NOT_FOUND);
+	}
+
+	public ResponseEntity<HttpStatus> deleteAllCustomers() {
+		customerRepository.deleteAll();
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+
+
 }
