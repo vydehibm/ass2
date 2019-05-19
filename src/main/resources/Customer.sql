9f49228f505357714bd2ff50147702b229c975f9
learner <learner@github.com>
2019-05-19 00:39:38 -0400

    Fixed data.sql filename

--- src/main/resources/Customer.sql
+++
@@ -1,2 +0,0 @@
-insert into Customer
-values(1, 'Vydehi', 1234565789, 'Milford, OH-45150', 'Female');
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into Customer
+values(1,'Milford, OH-45150',1234565789,'Vydehi','Female');
